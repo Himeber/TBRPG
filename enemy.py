48daee6f7aaf14cd77b58cd3446821bf695e80a2
George Busby <georgebusby08@gmail.com>
2024-11-05 15:00:22 -0700

    Update enemy.pie

--- enemy.py
+++ enemy.py
@@ -1,8 +1,10 @@
 from base_functions import *
 
 class Enemy:
-    def __init__(self,hp,atk,defence,xp):
+    def __init__(self,name,hp,atk,defence,xp):
         self.hp = hp
+        self.maxhp = hp
+        self.name = name
         self.atk = atk
         self.defence = defence
         self.xpreward = xp
@@ -13,8 +15,10 @@ class Enemy:
         return damage
     def damage(self,damage):
         self.hp -= int(damage)
+    def __repr__(self):
+        return f"{self.name} - {str(self.hp)}/{str(self.maxhp)} HP"
 class Boss(Enemy):
-    def __init__(self, hp, atk, defence, xp, abilities, rotation):
-        super().__init__(hp, atk, defence, xp)
+    def __init__(self,name, hp, atk, defence, xp, abilities, rotation):
+        super().__init__(name,hp, atk, defence, xp)
         self.abilities = abilities
         self.rotation = rotation
