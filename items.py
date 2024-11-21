ed611fd79d702e1af436dd99ae2c814b752a8dba
Himeber <george.busby@ucas-edu.net>
2024-11-21 11:46:57 -0700

    wweeeeeeeeeeeeeeeeee

--- items.py
+++ items.py
@@ -1,5 +1,6 @@
+from base_functions import *
 class Item:
-    def __init__(self,name,slot,hp=0,defence=0,mana=0,atk=0,special=None):
+    def __init__(self,name,slot,hp=0,defence=0,mana=0,matk=0,atk=0,special=None):
         self.name = name
         self.slot = slot
         self.hp = hp
@@ -7,7 +8,7 @@ class Item:
         self.mana = mana
         self.atk = atk
         self.special = special
-    
+        self.matk = matk
     def __repr__(self):
         returner = str(f"""{self.name} - {self.slot}
 Defence: {str(self.defence)} | Attack: {str(self.atk)}
@@ -18,3 +19,46 @@ HP: {str(self.hp)} | Mana: {str(self.mana)}""")
     
     def __str__(self):
         return str(self.__repr__())
+    
+class Chest:
+    def __init__(self,name="Chest",contents=[],money = 0) -> None:
+        self.contents = contents
+        self.money = money
+        self.name = name
+
+def generateItem(makeStats=True,itemhp=0,itematk=0,itemdef=0,itemmana=0,itemmatk=0):
+    pass
+
+def generateChest(you,mult=1):
+    scalar = you.level * mult * (random.random() + 0.5)
+    scalar *= 5
+    itemhp = 0
+    itematk = 0
+    itemdef = 0
+    itemmana = 0
+    itemmatk = 0
+    muns = 0
+    for i in range(scalar):
+        randomized = rng()
+        if randomized >= 0.9:
+            muns += round(random.random() * 100,2)
+        elif randomized >= 0.8:
+            itemhp += int((random.random()+0.5) * 5)
+        elif randomized >= 0.7:
+            itematk += int((random.random()) * 5)
+        elif randomized >= 0.6:
+            itemdef += int((random.random()) * 3)
+        elif randomized >= 0.5:
+            itemmana += int((random.random()+0.5) * 5)
+        elif randomized >= 0.4:
+            itemmatk += int((random.random()+0.5) * 5)
+        elif randomized >= 0.3:
+            itemhp += 1
+            itematk += 1
+            itemdef += 1
+            itemmana += 1
+            itemmatk += 1
+            muns += 1
+        else:
+            pass
+    item = generateItem(False,itemhp,itematk,itemdef,itemmana,itemmatk)
